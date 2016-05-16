fbf9c08a92d711a5e721daa39ec9e3b04e1584ff
Onam Chilwan <onamch@gmail.com>
2016-05-16 09:16:44 +0100

    Added a post operation (no unit test yet, naughty of me). Will fix on next commit

--- src/Nancy.Api/Modules/CustomerModule.cs
+++ src/Nancy.Api/Modules/CustomerModule.cs
@@ -1,6 +1,8 @@
 namespace Nancy.Api.Modules
 {
+    using Nancy.Api.Objects;
     using Nancy.Api.Queries;
+    using Nancy.ModelBinding;
     using Nancy.Responses.Negotiation;
 
     public class CustomerModule : NancyModule
@@ -12,6 +14,19 @@ namespace Nancy.Api.Modules
             this.getCustomerQuery = getCustomerQuery;
 
             this.Get["api/customers/{id:int}"] = args => this.GetCustomers(args.id);
+            this.Post["api/customers"] = args => this.AddCustomer();
+        }
+
+        private Negotiator AddCustomer()
+        {
+            var customer = this.Bind<Customer>();
+
+            if (customer == null)
+            {
+                return this.Negotiate.WithStatusCode(HttpStatusCode.BadRequest);
+            }
+
+            return this.Negotiate.WithStatusCode(HttpStatusCode.Created);
         }
 
         private Negotiator GetCustomers(int customerId)
